e306616a328bddcb958d92965bb04db3f682ca81
lichengru <gjo4us06c961115@gmail.com>
2019-10-24 20:29:58 +0800

    加入專案檔案。

---
+++ cale/cale.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cale
+{
+    public class cale
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+
+
+        public int sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
+
